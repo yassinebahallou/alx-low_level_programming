7b00e54379a23c25e132a669b0a01f91a063d031
yassinebahallou <erwinsmithsam@gmail.com>
2023-10-10 13:13:19 -0700

    i love, lain do you ?

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,81 @@
 #include "dog.h"
 #include <stdlib.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
 /**
-* new_dog - function that creates a new struct of type dog
-*
-* @name: parameter name as struct
-* @age: parameter age as struct
-* @owner: parameter owner as struct
-*
-* Return: returns pointer to new dog of datatype dog
-*/
+ * _strlen - Finds the length of a string
+ * @str: the string to be measured
+ * Return: the length of the string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcopy - copies a string pointed to by src including the
+ * terminating null byte, to a buffer pointed by dest
+ * @dest: the buffer storing the string copy
+ * @src: the source string
+ * Return: the pointer to dest
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Return: the new struct dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int new_len, blen, i;
-dog_t *lain;
+	dog_t *lain;
 
-new_len = blen = 0;
-while (name[new_len++])
-;
-while (owner[blen++])
-;
-lain = malloc(sizeof(dog_t));
-if (lain == NULL)
-return (NULL);
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
-lain->name = malloc(new_len *sizeof(lain->name));
-if (lain == NULL)
-return (NULL);
-for (i = 0; i < new_len; i++)
-lain->name[i] = name[i];
+	lain = malloc(sizeof(dog_t));
+	if (lain == NULL)
+		return (NULL);
 
-lain->age = age;
+	lain->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (lain->name == NULL)
+	{
+		free(lain);
+		return (NULL);
+	}
 
-lain->owner = malloc(blen * sizeof(lain->owner));
-if (lain == NULL)
-return (NULL);
-for (i = 0; i < blen; i++)
-lain->owner[i] = owner[i];
-return (lain);
+	lain->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (lain->owner == NULL)
+	{
+		free(lain->name);
+		free(lain);
+		return (NULL);
+	}
+	lain->name = _strcopy(lain->name, name);
+	lain->age = age;
+	lain->owner = _strcopy(lain->owner, owner);
+
+	return (lain);
 }
